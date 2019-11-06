8bf09c03e5d28a25245e44162b7ca0024a57b312
konosumi <36559308+konosumi@users.noreply.github.com>
2019-11-07 00:21:47 +0900

    Create ALDS1_1_D.cpp

---
+++ ALDS/ALDS1_1_D.cpp
@@ -0,0 +1,42 @@
+/**
+ * Maximum Profit
+ * http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_1_D&lang=ja
+ */
+
+#include<iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  // R = その時点の価格
+  // min = 最低価格
+  // profit = 利益
+  int R, min, profit;
+
+  for (int i = 0; i < n; i++) {
+    cin >> R;
+
+    // 1回目は最低価格を更新するだけで良い
+    if (i == 0) {
+      min = R;
+      continue;
+    }
+
+    // 2回目以降は利益計算を実施する
+    if (i == 1 || R - min > profit) {
+      profit = R - min;
+    }
+
+    // 最低価格を更新しているかどうか
+    if (R < min) {
+      min = R;
+    }
+  }
+
+  cout << profit << endl;
+
+  return 0;
+}
