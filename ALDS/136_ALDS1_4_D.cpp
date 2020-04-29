251932b01bbc138ec3926d83fa99dc80d819d9cb
konosumi <36559308+konosumi@users.noreply.github.com>
2020-04-29 16:01:13 +0900

    -

--- ALDS/136_ALDS1_4_D.cpp
+++ ALDS/136_ALDS1_4_D.cpp
@@ -7,31 +7,31 @@
 #include<vector>
 using namespace std;
 
-int calcV(vector <int>w, int k, int P) {
-  int sum = 0;
+bool canPackToTrack(vector <int>w, int k, int P) {
+  int capacity = 0;
 
   for (int i = 0; i < w.size(); i++) {
     // 最大積載量より大きい荷物は、そもそも積めない
     if (w.at(i) > P) {
-      return i;
+      return false;
     }
-    // 積載量を超えたら、トラックの数を減らせて、積載量をリセットする
-    if (sum + w.at(i) > P) {
+
+    // 積載量を超えたら、トラックの数を減らして積載量をリセットする
+    if (capacity + w.at(i) > P) {
       k--;
-      // cout << "----" << w.at(i) <<  "---" << P << "----" << sum << endl;
-      sum = 0;
+      capacity = 0;
     }
 
     // トラックがなくなってしまったら、もう荷物を積めない
     if (k == 0) {
-      // cout << "----" << endl;
-      return i;
+      return false;
     }
 
-    sum += w.at(i);
+    // トラックに荷物を積む
+    capacity += w.at(i);
   }
 
-  return w.size();
+  return true;
 }
 
 int main() {
@@ -39,6 +39,7 @@ int main() {
   int n, k;
   cin >> n >> k;
 
+  // 重さがそれぞれwのn個の荷物
   vector <int>w(n);
   for (int i = 0; i < n; i++) {
     cin >> w.at(i);
@@ -56,13 +57,13 @@ int main() {
    */
 
    // 2分探索
-   // 荷物の最大個数は100,000個で、最大サイズは10,000。これを最小のk=1だったとしても対応できる大きさが最小値となる
+   // 荷物の最大個数は100,000個で、最大サイズは10,000。これを超える十分に大きい最大積載量Pを最小値とする
    int min = 1, max = 100000 * 10000;
    int answer = 100000 * 10000;
    while (min < max) {
      int center = (min + max) / 2;
-     if (calcV(w, k, center) >= n) {
-       // centerより大きい積載量の値は計算する必要がない
+     if (canPackToTrack(w, k, center)) {
+       // centerより大きい積載量の値は、必ず積載可能なので計算する必要がない
        max = center;
        if (center < answer) {
          answer = center;
@@ -74,6 +75,4 @@ int main() {
    }
 
    cout << answer << endl;
-
-   return 0;
 }
