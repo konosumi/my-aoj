2e91aacf11dbb6576c84edab46271d3c01df1b19
konosumi <36559308+konosumi@users.noreply.github.com>
2020-04-29 17:12:51 +0900

    Create 146_ALDS1_5_C.cpp

---
+++ ALDS/146_ALDS1_5_C.cpp
@@ -0,0 +1,13 @@
+/**
+ * Koch Curve
+ * http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_5_C&lang=ja
+ */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+   // TODO: 
+
+}
